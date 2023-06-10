6b297fb7650107e6da9501b3f237e57102416319
Matthijs Kamstra <matthijs@matthijskamstra.nl>
2023-06-10 12:17:19 +0200

    add synfig

--- all.sh
+++ all.sh
@@ -16,3 +16,4 @@ echo "Rounded icons"
 sh _all_icon.sh design/export/icon_gimp_rounded.png gimp rounded
 sh _all_icon.sh design/export/icon_inkscape_rounded.png inkscape rounded
 sh _all_icon.sh design/export/icon_scribus_rounded.png scribus rounded
+sh _all_icon.sh design/export/icon_synfig_rounded.png synfig rounded
