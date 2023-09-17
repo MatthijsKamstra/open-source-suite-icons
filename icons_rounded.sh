8eacbcdec6f311176e0d650f17b2b6cf571728fe
Matthijs Kamstra <matthijs@matthijskamstra.nl>
2023-09-17 17:32:02 +0200

    overwrite more icons inkscape

--- icons_rounded.sh
+++ icons_rounded.sh
@@ -10,8 +10,13 @@ sh _all_icon.sh design/export/icon_synfig_rounded.png synfig rounded
 
 echo "Move Rounded icons"
 cp export/gimp/rounded/icon.icns /Applications/GIMP.app/Contents/Resources/gimp.icns
+# scribus
 cp export/scribus/rounded/icon.icns /Applications/Scribus.app/Contents/Resources/Scribus.icns
+# inkscape
 cp export/inkscape/rounded/icon.icns /Applications/Inkscape.app/Contents/Resources/inkscape.icns
+cp export/inkscape/rounded/icon.icns /Applications/Inkscape.app/Contents/Resources/icon.icns
+cp export/inkscape/rounded/icon.icns /Applications/Inkscape.app/Contents/Resources/Inkscape-Generic.icns
+# synfig studio
 cp export/synfig/rounded/icon.icns /Applications/SynfigStudio.app/Contents/Resources/SynfigStudio.icns
 
 echo "Icons set"
